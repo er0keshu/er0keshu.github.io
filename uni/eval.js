668e27290dbcd97cb8469fba662e1822f1283fd3
zhy <8737486+er0keshu@users.noreply.github.com>
2022-09-03 11:36:16 +0800

    注册eval钩子函数

--- uni/eval.js
+++ uni/eval.js
@@ -1,1 +1,22 @@
-console.log('evalJs')
+;(function () {
+  document.addEventListener('UniAppJSBridgeReady', function () {
+    const uw = uni.webView
+
+    let env = ''
+    uw.getEnv((res) => {
+      env = Object.keys(res).filter(function (k) {
+        return res[k]
+      })
+    })
+
+    const events = ['DOMContentLoaded', 'load', 'unload', 'hashchange'],
+      postMessage = (event, payload = {}) =>
+        uw.postMessage({ data: { event, payload } })
+
+    events.forEach((evt) => {
+      window.addEventListener(evt, () => {
+        postMessage(evt, { env, url: location.href, hash: location.hash })
+      })
+    })
+  })
+})()
